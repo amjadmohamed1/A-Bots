4d9d736295ab3b4232579f56d1217ca233e953e1
amjadmohamed1 <amjadmohamed.sd@gmail.com>
2026-07-06 13:41:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask
-import os
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,7 +6,14 @@ app = Flask(__name__)
 def home():
     return "A-Bots is running!"
 
+# هذا هو المسار الذي ستضعه في منصة Kick
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    print("بيانات جديدة من Kick:", data)
+    return "Received", 200
+
 if __name__ == '__main__':
-    # Railway يحدد المنفذ تلقائياً عبر متغير بيئي
+    import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
